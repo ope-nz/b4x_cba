65a9002b695d82abc6ce7a7a81016a67a01d416b
ope-nz <github@ope.nz>
2024-12-19 09:45:35 +1300

    Add files via upload

--- program.cs
+++ program.cs
@@ -4,26 +4,26 @@ using System.IO;
 using System.IO.Compression;
 using System.Reflection;
 using System.Runtime;
-//using System.Linq;
 using System.Text;
 
 namespace B4XCustomActions
 {
 	class Program
 	{
+		/// <summary>
+		/// Main entry point for the B4XCustomActions library.  This library is intended to be called from the command line
+		/// as a custom step in a B4X project.
+		/// </summary>
 		static void Main(string[] args)
 		{
 			try
 			{
-				//string ThisLocation = Directory.GetCurrentDirectory();
-
-				//Console.WriteLine(ThisLocation);
-				//Console.WriteLine(args.ToString());
+				// If the argument is a string, split it into an array of strings
+				if (args.Length == 1 && args[0].Contains(" ")) args = args[0].Split(' ');				
 
 				int c = args.GetUpperBound(0);
 
 				string TAction = "";
-				string TDirectory = "";
 				string TDateFormat = "yyyy-MM-dd";
 				string TTimeFormat = "HH:mm:ss";
 				string TSource = "";
@@ -32,7 +32,7 @@ namespace B4XCustomActions
 				// Loop through arguments
 				for (int n = 0; n < c; n++)
 				{
-					string thisKey = args[n].ToLower();
+					string thisKey = args[n].ToLower().TrimEnd().TrimStart();
 					string thisVal = args[n + 1].TrimEnd().TrimStart();
 
 					//Console.WriteLine(thisKey);
@@ -43,9 +43,6 @@ namespace B4XCustomActions
 					{
 						case "-action":
 							TAction = thisVal;
-							break;
-						case "-directory":
-							TDirectory = thisVal;
 							break;
 						case "-source":
 							TSource = thisVal;
@@ -62,9 +59,9 @@ namespace B4XCustomActions
 					}
 				}
 
-				Console.WriteLine("#####################");
-				Console.WriteLine("    B4X Custom Action");
-				Console.WriteLine("#####################");
+				Console.WriteLine("###############################################################");
+				Console.WriteLine("B4X Custom Action - " + GetProjectName());
+				Console.WriteLine("###############################################################");
 
 				switch (TAction)
 				{
@@ -74,7 +71,7 @@ namespace B4XCustomActions
 						break;
 					case "copyjar":
 						Console.WriteLine("Copy Jar");
-						CopyJar(TDirectory);
+						CopyJar(TDestination);
 						break;
 					case "copy":
 						Console.WriteLine("Copy");
@@ -92,6 +89,10 @@ namespace B4XCustomActions
 						Console.WriteLine("Zip");
 						Zip(TSource, TDestination);
 						break;
+					case "moveautobackups":
+						Console.WriteLine("Move AutoBackups");
+						MoveAutoBackups(TDestination);
+						break;
 					default:
 						Console.WriteLine("No action supplied");
 						break;
@@ -104,24 +105,40 @@ namespace B4XCustomActions
 			}
 		}
 
-		private static void CopyJar(string TargetDirectory)
+		/// <summary>
+		/// Copies a JAR file from the objects folder to the specified destination path.
+		/// </summary>
+		/// <param name="destinationPath">The target directory path where the JAR file will be copied.</param>
+		/// <remarks>If a JAR file is found, it is copied to the destination with overwriting enabled.
+		/// In case of an error during the process, an error message is printed, and the application exits with a status code of 1.</remarks>
+		private static void CopyJar(string destinationPath)
 		{
 			try
 			{
 				String jar = FindJar();
+
+				Console.WriteLine("Copying  " + jar + " to " + destinationPath);
+
 				if (jar != null)
 				{
-					File.Copy(Path.Combine(GetObjectsFolder(), jar), Path.Combine(TargetDirectory, jar), overwrite: true);
+					File.Copy(Path.Combine(GetObjectsFolder(), jar), Path.Combine(destinationPath, jar), overwrite: true);
 				}
 				Environment.Exit(0);
 			}
 			catch (Exception ex)
 			{
 				Console.WriteLine("An error occurred: " + ex.Message);
+				Console.WriteLine("destinationPath: " + destinationPath);
 				Environment.Exit(1);
 			}
 		}
 
+		/// <summary>
+		/// Writes the current date and time in the specified format to a file named "build.txt" in the Files folder.
+		/// </summary>
+		/// <param name="DateFormat">The format string for the date part of the DateTime object.</param>
+		/// <param name="TimeFormat">The format string for the time part of the DateTime object.</param>
+		/// <remarks>If a file with the same name already exists, it will be overwritten.</remarks>
 		private static void BuildTime(string DateFormat, string TimeFormat)
 		{
 			// Define the desired format for the DateTime string
@@ -139,19 +156,27 @@ namespace B4XCustomActions
 			catch (Exception ex)
 			{
 				Console.WriteLine("An error occurred: " + ex.Message);
+				Console.WriteLine("DateFormat: " + DateFormat);
+				Console.WriteLine("TimeFormat: " + TimeFormat);
 				Environment.Exit(1);
 			}
 		}
 
+		/// <summary>
+		/// Updates the version number in a file named "version.txt" in the Files folder.
+		/// </summary>
+		/// <remarks>
+		/// The version number is incremented by one. If the build number exceeds 9, the minor version is incremented, and if the minor version exceeds 9, the major version is incremented.
+		/// If a file with the same name already exists, it will be overwritten.
+		/// </remarks>
 		private static void UpdateVersion()
 		{
+			// Default version
+			string version = "0.0.1";
+			string TargetFile = Path.Combine(GetFilesFolder(), "version.txt");
+
 			try
 			{
-				string TargetFile = Path.Combine(GetFilesFolder(), "version.txt");
-
-				// Default version
-				string version = "0.0.1";
-
 				if (File.Exists(TargetFile))
 				{
 					// Read the current version from the file
@@ -181,6 +206,8 @@ namespace B4XCustomActions
 					version = major + "." + minor + "." + build;
 				}
 
+				Console.WriteLine("Incrementing version to " + version);
+
 				// Write the updated version back to the file
 				File.WriteAllText(TargetFile, version);
 				Environment.Exit(0);
@@ -188,16 +215,30 @@ namespace B4XCustomActions
 			catch (Exception ex)
 			{
 				Console.WriteLine("An error occurred: " + ex.Message);
+				Console.WriteLine("version: " + version);
+
+				if (File.Exists(TargetFile))
+				{
+					// Read the current version from the file
+					Console.WriteLine("version.txt: " + File.ReadAllText(TargetFile).Trim());
+				}
+
 				Environment.Exit(1);
 			}
 		}
 
+		/// <summary>
+		/// Copies the specified source path to the specified destination path.
+		/// </summary>
+		/// <param name="sourcePath">The source path to copy. Can be a file or a directory. If it is a relative path, it will be resolved relative to the objects folder.</param>
+		/// <param name="destinationPath">The destination path to copy to. If it is a relative path, it will be resolved relative to the files folder.</param>
+		/// <remarks>If a file with the same name already exists in the destination path, it will be overwritten.</remarks>
 		public static void CopyPath(string sourcePath, string destinationPath)
 		{
 			try
 			{
-				if (sourcePath.StartsWith("Files")) sourcePath = Path.Combine(GetFilesFolder(),sourcePath.Substring(5));
-				if (destinationPath.StartsWith("Files")) destinationPath = Path.Combine(GetFilesFolder(),destinationPath.Substring(5));
+				if (sourcePath.StartsWith("Files")) sourcePath = Path.Combine(GetFilesFolder(), sourcePath.Substring(5));
+				if (destinationPath.StartsWith("Files")) destinationPath = Path.Combine(GetFilesFolder(), destinationPath.Substring(5));
 
 				if (!sourcePath.Contains(":"))
 				{
@@ -236,16 +277,24 @@ namespace B4XCustomActions
 			catch (Exception ex)
 			{
 				Console.WriteLine("An error occurred: " + ex.Message);
+				Console.WriteLine("sourcePath: " + sourcePath);
+				Console.WriteLine("destinationPath: " + destinationPath);
 				Environment.Exit(1);
 			}
 		}
 
+		/// <summary>
+		/// Zips the specified source path to the specified destination path.
+		/// </summary>
+		/// <param name="sourcePath">The source path to zip. Can be a file or a directory. If it is a relative path, it will be resolved relative to the objects folder.</param>
+		/// <param name="destinationPath">The destination path to zip to. If it is a relative path, it will be resolved relative to the files folder.</param>
+		/// <remarks>If a file with the same name already exists in the destination path, it will be overwritten. If destinationPath does not end with .zip, the zip file name will be derived from the source name.</remarks>
 		private static void Zip(string sourcePath, string destinationPath)
 		{
 			try
 			{
-				if (sourcePath.StartsWith("Files")) sourcePath = Path.Combine(GetFilesFolder(),sourcePath.Substring(5));
-				if (destinationPath.StartsWith("Files")) destinationPath = Path.Combine(GetFilesFolder(),destinationPath.Substring(5));
+				if (sourcePath.StartsWith("Files")) sourcePath = Path.Combine(GetFilesFolder(), sourcePath.Substring(5));
+				if (destinationPath.StartsWith("Files")) destinationPath = Path.Combine(GetFilesFolder(), destinationPath.Substring(5));
 
 				// If no drive then assume its a relative path, append base directory
 				if (!sourcePath.Contains(":")) sourcePath = Path.Combine(GetObjectsFolder(), sourcePath);
@@ -278,7 +327,6 @@ namespace B4XCustomActions
 					// Source is a file
 					using (ZipStorer zip = ZipStorer.Create(zipFilePath))
 					{
-						Console.WriteLine("JHello");
 						zip.AddFile(ZipStorer.Compression.Deflate, sourcePath, Path.GetFileName(sourcePath));
 					}
 
@@ -298,10 +346,17 @@ namespace B4XCustomActions
 			catch (Exception ex)
 			{
 				Console.WriteLine("An error occurred: " + ex.Message);
+				Console.WriteLine("sourcePath: " + sourcePath);
+				Console.WriteLine("destinationPath: " + destinationPath);
 				Environment.Exit(1);
 			}
 		}
 
+		/// <summary>
+		/// Search for .jar files in the directory
+		/// and return the first .jar filename if found, otherwise return null
+		/// </summary>
+		/// <returns>The first .jar filename if found, otherwise null</returns>
 		private static String FindJar()
 		{
 			// Search for .jar files in the directory
@@ -311,17 +366,63 @@ namespace B4XCustomActions
 			return jarFiles.Length > 0 ? Path.GetFileName(jarFiles[0]) : null;
 		}
 
+		/// <summary>
+		/// Gets the path of the Files folder.
+		/// </summary>
+		/// <returns>The path of the Files folder.</returns>
+		/// <remarks>
+		/// This method returns the path of the Files folder, which is located in the same directory as the Objects folder.
+		/// </remarks>
 		private static string GetFilesFolder()
 		{
 			string Files = Path.GetDirectoryName(GetObjectsFolder());
 			return Path.Combine(Files, "Files");
 		}
 
+		/// <summary>
+		/// Gets the current working directory of the application.
+		/// </summary>
+		/// <returns>The path of the current working directory.</returns>
 		private static string GetObjectsFolder()
 		{
 			return Directory.GetCurrentDirectory();
 		}
 
+		/// <summary>
+		/// Gets the path of the project folder.
+		/// </summary>
+		/// <returns>The path of the project folder.</returns>
+		private static string GetProjectFolder()
+		{
+			return Path.GetDirectoryName(GetObjectsFolder());
+		}
+
+		/// <summary>
+		/// Retrieves the name of the project file in the project folder.
+		/// </summary>
+		/// <returns>The project name without the file extension, or "unknown" if no project file is found.</returns>
+		/// <remarks>
+		/// This method searches for files with the ".b4?" extension in the project folder and returns the name of the first match found.
+		/// If no such files are present, it defaults to returning "unknown".
+		/// </remarks>
+		private static string GetProjectName()
+		{
+			string[] projectFiles = Directory.GetFiles(GetProjectFolder(), "*.b4?");
+			string project = null;
+			if (projectFiles.Length > 0) project = Path.GetFileName(projectFiles[0]);
+			if (project != null) return project.Substring(0, project.LastIndexOf("."));
+			return "unknown";
+		}
+
+		/// <summary>
+		/// Copies a file to a destination file.
+		/// </summary>
+		/// <param name="sourceFile">The path of the file to copy.</param>
+		/// <param name="destinationFile">The path of the file to copy to.</param>
+		/// <remarks>
+		/// If the destination directory does not exist, it is created.
+		/// If a file with the same name already exists at the destination, it is overwritten.
+		/// </remarks>
 		private static void CopyFile(string sourceFile, string destinationFile)
 		{
 			string destinationDir = Path.GetDirectoryName(destinationFile);
@@ -334,6 +435,65 @@ namespace B4XCustomActions
 			File.Copy(sourceFile, destinationFile, overwrite: true);
 		}
 
+		/// <summary>
+		/// Moves all B4A AutoBackup zip files to a specified destination directory.
+		/// </summary>
+		/// <param name="destinationDir">The destination directory to move the zip files to.</param>
+		/// <remarks>
+		/// This method moves all zip files from the AutoBackups folder in the project directory
+		/// to the destination directory and renames them to include the project name.
+		/// The destination directory is created if it does not already exist.
+		/// The method will exit the program with a status code of 0 if no errors occur, or 1 if an error occurs.
+		/// </remarks>
+		private static void MoveAutoBackups(string destinationDir)
+		{
+			try
+			{
+				string sourceDir = Path.Combine(GetProjectFolder(), "AutoBackups");
+
+				destinationDir = Path.Combine(destinationDir, GetProjectName());
+
+				if (!Directory.Exists(destinationDir))
+				{
+					Directory.CreateDirectory(destinationDir);
+				}
+
+				if (Directory.Exists(sourceDir))
+				{
+					string[] zipFiles = Directory.GetFiles(sourceDir, "*.zip");
+
+					// Copy each .zip file to the destination directory
+					foreach (string file in zipFiles)
+					{
+						string fileName = Path.GetFileName(file);
+						string destinationPath = Path.Combine(destinationDir, fileName);
+
+						// Copy file and overwrite if it already exists
+						File.Move(file, destinationPath);
+						Console.WriteLine("Copied: " + fileName);
+					}
+				}
+
+				Environment.Exit(0);
+			}
+
+			catch (Exception ex)
+			{
+				Console.WriteLine("An error occurred: " + ex.Message);
+				Environment.Exit(1);
+			}
+
+		}
+
+		/// <summary>
+		/// Copy all files and subdirectories from the source directory to the destination directory.
+		/// If the destination directory does not exist, it will be created.
+		/// All files in the source directory will be copied to the destination directory.
+		/// All subdirectories in the source directory will be copied to the destination directory
+		/// using a recursive call to this method.
+		/// </summary>
+		/// <param name="sourceDir">The source directory to copy from.</param>
+		/// <param name="destinationDir">The destination directory to copy to.</param>
 		private static void CopyDirectory(string sourceDir, string destinationDir)
 		{
 			// Create destination directory if it does not exist
